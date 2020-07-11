f05d3297bc7cd920424f47e90e20fb208646cb76
vip <a1h2med>
2020-07-11 03:32:14 +0200

    I Am Rich App

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey,
+      ),
+    body: Center(
+      child: Image(image: AssetImage('images/diamond.jfif')),
+    ),),
+  ));
+}
